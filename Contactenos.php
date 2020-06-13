843d8db74739cc1285fc3d5810032af755156018
David Fagua <48558001+David-Fagua@users.noreply.github.com>
2020-06-13 00:52:05 -0500

    v0.0.5

---
+++ Contactenos.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Iniciar Sesión</title>
+
+  <link rel="icon" type="image/png" href="../Img/Logo.png">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+</head>
+
+<body>
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div>
+  <nav class="navbar navbar-light navbar-expand-lg fixed-top" style="background-color: #47A5B4;">
+    <a href="index.php" class="navbar-brand" >
+      <img src="assets/img/Logo.png" width="40" height="40" alt="HSD PLUS">
+    </a>
+    <a href="index.php" class="navbar-brand" >HSD PLUS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+          <a href="Catalogo.php" class="nav-link" style="color: #ffffff;">Catalogo
+          </a>
+          <a href="Contactenos.php" class="nav-link" style="color: #ffffff;">Contactenos
+          </a>
+          <a href="Quienessomos.php" class="nav-link" style="color: #ffffff;">¿Quienes Somos?
+          </a>
+          <a href="Misión y Visión.php" class="nav-link" style="color: #ffffff;">Misión y Visión
+          </a>
+          <a href="Portafolio.php" class="nav-link" style="color: #ffffff;">Portafolio
+          </a>
+        </li>
+      </ul>
+      
+      <div>
+        <button type="button" class="btn btn-info">
+          <a href="start.php"  >
+            <img src="assets/img/ingles.png" width="40" height="40" alt="">
+          </a>
+        </button>
+      </div>
+      
+      &nbsp;
+      &nbsp;
+
+      <div class="btn-group dropleft">
+      <button type="button" class="btn btn-info" class="btn btn-sec dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <img src="assets/img/Usuario.png" width="40" height="40" alt="">
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="./sesion/iniciosesion.php">Iniciar Sesión</a>
+        <a class="dropdown-item" href="./sesion/Registrar.php">Registrar</a>
+      </div>
+      </div>
+
+    </div>
+  </nav>
+</div>
+
+<center>
+<div class="container">
+  <div class="row featurette">
+    <div>
+      <img src="assets/img/en proceso.png"  class="card-img-top" alt="img">
+    </div>
+  </div>
+</div>
+</center>
+
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
